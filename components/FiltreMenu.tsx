13175fdef185af3015caeb1667aeef1406cc5df2
Fred120-code <fredayemtsa@gmail.com>
2025-08-27 15:39:37 +0100

    mise en place du responsive

--- components/FiltreMenu.tsx
+++ components/FiltreMenu.tsx
@@ -8,12 +8,13 @@ type Props = {
 
 const FiltreMenu = ({ onCategoryChange }: Props) => {
   return (
-    <div className="flex items-center justify-between bg-gray-300 w-full p-2 rounded-md">
+    <div className="lg:flex lg:flex-row lg:items-center lg:justify-between bg-gray-300 w-full p-2 rounded-md
+                        sm:flex-col sm:flex sm:justify-center sm:items-center">
       <div>
         <h1 className="text-xl font-bold">Photo Galerie</h1>
       </div>
       <div>
-        <ul className="flex gap-6">
+        <ul className="md:flex md:flex-row gap-6 sm:flex sm:flex-col">
           <button
             className="text-xl font-bold text-white bg-blue-300 p-1 m-1 rounded-xl cursor-pointer"
             onClick={() => onCategoryChange("all")}
--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -12,10 +12,10 @@ type Props = {
 };
 const Gallery = ({ images }: Props) => {
   return (
-    <div className="grid grid-cols-3 gap-6 mt-6">
+    <div className="grid lg:grid-cols-3 gap-6 mt-6">
       {images.map((img) => (
-        <div key={img.id} className="rounded-md shadow-md overflow-hidden">
-          <img src={img.url} alt={img.title} className="w-full h-auto" />
+        <div key={img.id} className="rounded-md shadow-md overflow-hidden lg:h-full sm:h-[70rem] sm:w-full">
+          <img src={img.url} alt={img.title} className="w-full lg:h-auto sm:h-1/2" />
           <p className="text-center font-semibold">{img.title}</p>
         </div>
       ))}
